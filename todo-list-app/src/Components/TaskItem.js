a0e22c012defdfa3b2fea30fbc89190d1227afcc
Turlinahi <ioanacristinastef@gmail.com>
2023-11-18 15:36:11 +0200

    CommentsOfCss

--- todo-list-app/src/Components/TaskItem.js
+++ todo-list-app/src/Components/TaskItem.js
@@ -8,7 +8,7 @@ const TaskItem = ({ task, deleteTask, toggleTaskCompletion }) => {
         <button onClick={() => toggleTaskCompletion(task.id)}>
           {task.completed ? 'Undo' : 'Complete'}
         </button>
-        <button onClick={() => deleteTask(task.id)}>Delete</button>
+        <button class="delete" onClick={() => deleteTask(task.id)}>Delete</button>
       </div>
     </li>
   );
